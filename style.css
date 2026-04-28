cd22609286b08850e6288f20913a5ac2d8a07c16
BASHEER BURHAN <bashkokurdi6@gmail.com>
2026-04-28 16:49:09 +0300

    Update style.css

--- style.css
+++ style.css
@@ -1,1 +1,51 @@
+body {
+  margin:0;
+  font-family:Arial;
+  background:#0f172a;
+  color:white;
+}
 
+header {
+  background:#16a34a;
+  padding:15px;
+  text-align:center;
+  font-size:22px;
+}
+
+nav {
+  display:flex;
+  justify-content:space-around;
+  background:#020617;
+  padding:10px;
+}
+
+nav button {
+  background:none;
+  border:none;
+  color:white;
+  cursor:pointer;
+}
+
+section {
+  display:none;
+  padding:20px;
+}
+
+.active {
+  display:block;
+}
+
+input {
+  display:block;
+  margin:10px 0;
+  padding:10px;
+  width:100%;
+}
+
+button {
+  padding:10px;
+  background:#22c55e;
+  border:none;
+  color:white;
+  cursor:pointer;
+}
